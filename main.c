97c53f9c3488f5f423786cf188528affb3fcc3b4
mucahittanacioglu <mucahittanacioglu@users.noreply.github.com>
2019-11-27 05:19:54 +0300

    ss

--- main.c
+++ main.c
@@ -1,53 +1,212 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct BinarySearchTree BST;
-struct BinarySearchTree{
-    int key;
-    BST* left;
-    BST* right;
 
+struct node
+{
+    int data; //node will store an integer
+    struct node *right_child; // right child
+    struct node *left_child; // left child
 };
 
-void addToTree(int temp, BST **pTree);
-
-int main() {
-    int temp;
-    BST* root=NULL;
-
-    while(temp!= -1){
-        scanf("%d",&temp);
-        getchar();
-        if(temp == -1)
-            break;
-        addToTree(temp,&root);
-    }
-    return 0;
+struct node* search(struct node *root, int x)
+{
+    if(root==NULL || root->data==x) //if root->data is x then the element is found
+        return root;
+    else if(x>root->data) // x is greater, so we will search the right subtree
+        return search(root->right_child, x);
+    else //x is smaller than the data, so we will search the left subtree
+        return search(root->left_child,x);
 }
 
-void addToTree(int key, BST **root) {
-    BST *iter= *root;
-    if((*root)==NULL){
-        (*root)=malloc(sizeof(BST));
-        (*root)->key=key;
-        (*root)->left=NULL;
-        (*root)->right=NULL;
-        return;
+//function to find the minimum value in a node
+struct node* find_minimum(struct node *root)
+{
+    if(root == NULL)
+        return NULL;
+    else if(root->left_child != NULL) // node with minimum value will have no left child
+        return find_minimum(root->left_child); // left most element will be minimum
+    return root;
+}
+
+//function to create a node
+struct node* new_node(int x)
+{
+    struct node *p;
+    p = malloc(sizeof(struct node));
+    p->data = x;
+    p->left_child = NULL;
+    p->right_child = NULL;
+
+    return p;
+}
+
+struct node* insert(struct node *root, int x)
+{
+    //searching for the place to insert
+    if(root==NULL)
+        return new_node(x);
+    else if(x>root->data) // x is greater. Should be inserted to right
+        root->right_child = insert(root->right_child, x);
+    else // x is smaller should be inserted to left
+        root->left_child = insert(root->left_child,x);
+    return root;
+}
+
+// funnction to delete a node
+struct node* delete(struct node *root, int x)
+{
+    //searching for the item to be deleted
+    if(root==NULL)
+        return NULL;
+    if (x>root->data)
+        root->right_child = delete(root->right_child, x);
+    else if(x<root->data)
+        root->left_child = delete(root->left_child, x);
+    else
+    {
+        //No Children
+        if(root->left_child==NULL && root->right_child==NULL)
+        {
+            free(root);
+            return NULL;
+        }
+
+            //One Child
+        else if(root->left_child==NULL || root->right_child==NULL)
+        {
+            struct node *temp;
+            if(root->left_child==NULL)
+                temp = root->right_child;
+            else
+                temp = root->left_child;
+            free(root);
+            return temp;
+        }
+
+            //Two Children
+        else
+        {
+            struct node *temp = find_minimum(root->right_child);
+            root->data = temp->data;
+            root->right_child = delete(root->right_child, temp->data);
+        }
     }
-    while(key < iter->key && iter->left != NULL )
-        iter = iter->left;
-    while(key > iter->key && iter->right != NULL )
-        iter=iter->right;
-    if(key < iter->key) {
-        iter->left = malloc(sizeof(BST));
-        iter->left->key=key;
-        iter->left->left=NULL;
-        iter->left->right=NULL;
-        return;
+    return root;
+}
+
+void inorder(struct node *root)
+{
+    if(root!=NULL) // checking if the root is not null
+    {
+        inorder(root->left_child); // visiting left child
+        printf(" %d ", root->data); // printing data at root
+        inorder(root->right_child);// visiting right child
     }
-    if(key > iter->key) {
-        iter->right = malloc(sizeof(BST));
-        iter->right->key=key;
-        iter->right->left=NULL;
-        iter->right->right=NULL;
+}
+
+int main()
+{
+    /*
+                   20
+                 /    \
+                /      \
+               5       30
+             /   \     /\
+            /     \   /  \
+           1      15 25  40
+                /          \
+               /            \
+              9             45
+            /   \          /
+           /     \        /
+          7      12      42
+    */
+    struct node *root;
+    int temp =11;
+    int initRoot = 0;
+    while(temp != -1){
+        scanf("%d",&temp);
+        getchar();
+        if(initRoot == 0){
+            root = new_node(temp);
+            initRoot=1;
+        }
+        if(temp == -1)
+            break;
+        insert(root,temp);
     }
+
+    inorder(root);
+    printf("\n");
+
+    root = delete(root, 1);
+    /*
+                   20
+                 /    \
+                /      \
+               5       30
+                 \     /\
+                  \   /  \
+                  15 25   40
+                /           \
+               /             \
+              9              45
+            /   \           /
+           /     \         /
+          7      12       42
+    */
+
+    root = delete(root, 40);
+    /*
+                   20
+                 /    \
+                /      \
+               5       30
+                 \     /\
+                  \   /  \
+                  15 25  45
+                 /       /
+                /       /
+               9       42
+             /   \
+            /     \
+           7      12
+    */
+
+    root = delete(root, 45);
+    /*
+                   20
+                 /    \
+                /      \
+               5       30
+                 \     /\
+                  \   /  \
+                  15 25  42
+                 /
+                /
+               9
+             /   \
+            /     \
+           7      12
+    */
+    root = delete(root, 9);
+    inorder(root);
+    /*
+                   20
+                 /    \
+                /      \
+               5       30
+                 \     /\
+                  \   /  \
+                  15 25  42
+                 /
+                /
+               12
+             /
+            /
+           7
+    */
+    printf("\n");
+
+    return 0;
 }
